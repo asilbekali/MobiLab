351e823b8eb9a44787d99eeba40fbc428dc8abd2
asilbekali <asilbekabdugafforov7@gmail.com>
2026-02-25 18:07:03 +0500

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,43 +4,29 @@ import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
 import "./globals.css";
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const geist = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
 
-// 1. SEO va Google uchun kuchli Meta Data
 export const metadata: Metadata = {
   title: "Mobilografiya Kursi | SHOXJAXON Ahmedov - Professional Video Montaj",
   description:
-    "Mobilografiya kurslari - telefon orqali professional reels, reklama va tijoriy videolar olishni SHOXJAXON Ahmedovdan o'rganing. SMM va sifatli kontent yaratish sirlari.",
+    "Mobilografiya kurslari - telefon orqali professional reels, reklama va tijoriy videolar olishni SHOXJAXON Ahmedovdan o'rganing.",
   keywords: [
     "mobilografiya",
-    "mobilografiya kursi",
     "shoxjaxon axmedov",
-    "shoxjahon ahmedov",
     "reels olish",
     "video montaj kursi",
-    "SMM video",
-    "telefon montaj",
-    "mobil video",
-    "reels sirlari",
   ],
   authors: [{ name: "SHOXJAXON Ahmedov" }],
-  creator: "SHOXJAXON Ahmedov",
-  publisher: "SHOXJAXON Ahmedov",
-  robots: "index, follow",
   openGraph: {
     type: "website",
     locale: "uz_UZ",
-    url: "https://shoxjaxonaxmedov.uz", // O'z domeningizni yozing
+    url: "https://shoxjaxonaxmedov.uz",
     title: "SHOXJAXON Ahmedov | Professional Mobilografiya Maktabi",
-    description:
-      "Noldan professional darajagacha mobilografiya kurslari. Bugun o'rganing, ertaga daromad qiling!",
-    siteName: "SHOXJAXON Ahmedov Mobilografiya",
-    images: [{ url: "/og-image.jpg" }], // Ijtimoiy tarmoqlar uchun rasm
-  },
-  icons: {
-    icon: "/icon.svg",
-    apple: "/apple-icon.png",
+    images: [{ url: "/og-image.jpg" }],
   },
 };
 
@@ -53,11 +39,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="uz">
-      <body className="font-sans antialiased bg-black">
-        {children}
-        <Analytics />
-
-        {/* Meta Pixel - Body oxirida (Hydration xatosi bermasligi uchun) */}
+      <head>
+        {/* Meta Pixel Script */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -68,18 +51,27 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '3178375402339774'); // Pixel ID here !
+            fbq('init', '3178375402339774');
             fbq('track', 'PageView');
           `}
         </Script>
+      </head>
+      <body
+        className={`${geist.variable} ${geistMono.variable} antialiased bg-black text-white`}
+      >
+        {/* Noscript qismi JS o'chiq bo'lgan holatlar uchun */}
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=3178375402339774&ev=PageView&noscript=1"
+            alt="facebook pixel"
           />
         </noscript>
+
+        {children}
+        <Analytics />
       </body>
     </html>
   );
